d8650c18eb5ab0b0325e6583e5b5909c835b77e9
jusoaresg <julianosgreg@gmail.com>
2024-12-20 17:08:13 -0300

    feat(fzf-lua): change telescope to fzf-lua
    BREAKING CHANGES: remove telescope from the project and change all the
    keybinds to use the fzf-lua

---
+++ lua/custom/plugins/fzf.lua
@@ -0,0 +1,27 @@
+return {
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- calling `setup` is optional for customization
+		require("fzf-lua").setup({})
+		local fzf = require("fzf-lua")
+		vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>sw", fzf.grep, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
+
+		vim.keymap.set("n", "<leader>sW", fzf.grep_cWORD, { desc = "[S]earch current [W]ord" })
+
+		vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics Document" })
+		vim.keymap.set("n", "<leader>sD", fzf.diagnostics_workspace, { desc = "[S]earch [D]iagnostics Workspace" })
+
+		vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
+
+		vim.keymap.set("n", "<C-p>", fzf.git_files, { desc = "[ ] Git files" })
+	end,
+}
