8d65ca83631fe095c58376933e1ba73e0e56ebd8
jusoaresg <julianosgreg@gmail.com>
2024-12-20 17:05:55 -0300

    feat(file-manager) add columns with permissions, size and mtime
    If devicons installed it will use icons on files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -7,6 +7,12 @@ return {
 			-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
 			-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
 			default_file_explorer = true,
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+				"mtime",
+			},
 			-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
 			skip_confirm_for_simple_edits = true,
 
@@ -14,6 +20,7 @@ return {
 				-- Show files and directories that start with "."
 				show_hidden = true,
 			},
+
 			-- NOTE: VIM Oil config (File explorer)
 			vim.api.nvim_create_user_command("Ex", "Oil <args>", { nargs = "?", complete = "dir" }),
 			vim.api.nvim_create_user_command("E", "Oil <args>", { nargs = "?", complete = "dir" }),
