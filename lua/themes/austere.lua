dcbbf613504cd787012cf4e8f6aaf3abaec5c44c
jusoaresg <julianosgreg@gmail.com>
2024-12-20 17:02:01 -0300

    feat(theme): change default theme from austere to warlock
    Add warlock theme

--- lua/themes/austere.lua
+++ lua/themes/austere.lua
@@ -3,6 +3,6 @@ return {
 	"LuRsT/austere.vim",
 	init = function()
 		vim.g.dark_transp_bg = 1
-		vim.cmd("colorscheme austere")
+		-- vim.cmd("colorscheme austere")
 	end,
 }
---
+++ lua/themes/warlock.lua
@@ -0,0 +1,7 @@
+return {
+	-- "ryanpcmcquen/true-monochrome_vim",
+	"hardselius/warlock",
+	init = function()
+		vim.cmd("colorscheme warlock")
+	end,
+}
