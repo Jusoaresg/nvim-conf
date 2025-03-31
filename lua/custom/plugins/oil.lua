6e525d5d62a4e2ece70c679c4149ea803e200c36
jusoaresg <julianosgreg@gmail.com>
2025-03-30 22:36:35 -0300

    feat: activate oil nvim as file manager remove: dired plugin feat: add keymap for lsp_definitions "<leader>cd"

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,30 +1,30 @@
 return {
-	-- -- NOTE: File Manager OIL
-	--
-	-- "stevearc/oil.nvim",
-	-- config = function()
-	-- 	require("oil").setup({
-	-- 		-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-	-- 		-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-	-- 		default_file_explorer = true,
-	-- 		columns = {
-	-- 			"icon",
-	-- 			"permissions",
-	-- 			"size",
-	-- 			"mtime",
-	-- 		},
-	-- 		-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
-	-- 		skip_confirm_for_simple_edits = true,
-	--
-	-- 		view_options = {
-	-- 			-- Show files and directories that start with "."
-	-- 			show_hidden = true,
-	-- 		},
-	--
-	-- 		-- NOTE: VIM Oil config (File explorer)
-	-- 		vim.api.nvim_create_user_command("Ex", "Oil <args>", { nargs = "?", complete = "dir" }),
-	-- 		vim.api.nvim_create_user_command("E", "Oil <args>", { nargs = "?", complete = "dir" }),
-	-- 		vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "[O]il File Manager" }),
-	-- 	})
-	-- end,
+	-- NOTE: File Manager OIL
+
+	"stevearc/oil.nvim",
+	config = function()
+		require("oil").setup({
+			-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+			-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+			default_file_explorer = true,
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+				"mtime",
+			},
+			-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+			skip_confirm_for_simple_edits = true,
+
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+
+			-- NOTE: VIM Oil config (File explorer)
+			vim.api.nvim_create_user_command("Ex", "Oil <args>", { nargs = "?", complete = "dir" }),
+			vim.api.nvim_create_user_command("E", "Oil <args>", { nargs = "?", complete = "dir" }),
+			vim.keymap.set("n", "<leader>o-", "<cmd>Oil<cr>", { desc = "[O]il File Manager" }),
+		})
+	end,
 }
