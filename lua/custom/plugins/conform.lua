a8f8866a1c416726afb3d8d55cfbe70e71c12a73
jusoaresg <julianosgreg@gmail.com>
2025-06-26 01:18:55 -0300

    fix>: conform for js feat: disable conform for js

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true, svelte = true, html = true, templ = true, js = true }
+				local disable_filetypes = { c = true, cpp = true, svelte = true, html = true, templ = true }
 				return {
 					timeout_ms = 500,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -31,7 +31,7 @@ return {
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { { "prettierd", "prettier" } },
+				javascript = { "prettierd", "prettier" },
 			},
 		},
 	},
