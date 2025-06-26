4df3ba3b5d2d7541b129159e0ec13cd3db57f160
jusoaresg <julianosgreg@gmail.com>
2025-06-26 01:18:16 -0300

    feat: add commentstrings for svelte and templ

--- lua/custom/comments.lua
+++ lua/custom/comments.lua
@@ -1,8 +1,17 @@
 -- NOTE: Comment strings
+return {
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "templ",
-	callback = function()
-		vim.bo.commentstring = "// %s"
-	end,
-})
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "templ",
+		callback = function()
+			vim.bo.commentstring = "// %s"
+		end,
+	}),
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "svelte",
+		callback = function()
+			vim.bo.commentstring = "<!-- %s -->"
+		end,
+	}),
+}
