89bdb0d9353ddb6ca3ff48e7e3da7530074dc9eb
jusoaresg <julianosgreg@gmail.com>
2025-06-26 01:17:14 -0300

    feat: add bigfile support on snacks plugin

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
 		"folke/snacks.nvim",
 		---@type snacks.Config
 		opts = {
+			bigfile = {},
 			picker = {
 				-- your picker configuration comes here
 				-- or leave it empty to use the default settings
