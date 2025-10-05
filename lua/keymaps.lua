488074e6ed4228f837915d77d7db137c473e3ab9
jusoaresg <julianosgreg@gmail.com>
2025-10-04 22:09:05 -0300

    feat(keymaps): add silent on archive executable keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Exit to file tree" })
 -- Killing actual buffer
 vim.keymap.set("n", "<leader>bk", "<cmd>bd<CR>", { desc = "Kill current buffer" })
 
-vim.keymap.set("n", "<leader>ax", "<cmd>!chmod +x %<CR>", { desc = "Executable" })
+vim.keymap.set("n", "<leader>ax", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Executable" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
