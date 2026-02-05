9343caa8aecfe15de41e2f2f2ecc9c57b520f63f
jusoaresg <julianosgreg@gmail.com>
2026-02-05 18:19:05 -0300

    feat(oil): change keybind to open oil file manager

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -24,7 +24,7 @@ return {
 			-- NOTE: VIM Oil config (File explorer)
 			vim.api.nvim_create_user_command("Ex", "Oil <args>", { nargs = "?", complete = "dir" }),
 			vim.api.nvim_create_user_command("E", "Oil <args>", { nargs = "?", complete = "dir" }),
-			vim.keymap.set("n", "<leader>o-", "<cmd>Oil<cr>", { desc = "[O]il File Manager" }),
+			vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "[O]il File Manager" }),
 		})
 	end,
 }
