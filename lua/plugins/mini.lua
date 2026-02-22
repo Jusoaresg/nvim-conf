51a1f71a38c1b0953c1bca51419f295b558ffa92
jusoaresg <julianosgreg@gmail.com>
2026-02-22 02:45:25 -0300

    feat(plugins): add mini.splitjoin and remove treejs

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-mini/mini.splitjoin",
+	version = "*",
+	config = function()
+		local splitjoin = require("mini.splitjoin")
+		splitjoin.setup({
+			mappings = {
+				toggle = "m",
+				split = "",
+				join = "",
+			},
+		})
+	end,
+}
