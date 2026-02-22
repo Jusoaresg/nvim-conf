e407384ed9e037cb50db425f2ec63159b2512b5b
jusoaresg <julianosgreg@gmail.com>
2026-02-22 02:48:12 -0300

    feat(plugins): starting changing from fzf-lua to telescope

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -40,24 +40,24 @@ return {
 		})
 		local fzf = require("fzf-lua")
 
-		vim.keymap.set("n", "<leader>cd", fzf.lsp_definitions, { desc = "[C]ode [D]efinition" })
+		-- vim.keymap.set("n", "<leader>cd", fzf.lsp_definitions, { desc = "[C]ode [D]efinition" })
 
-		vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", fzf.grep, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
-
-		vim.keymap.set("n", "<leader>sW", fzf.grep_cWORD, { desc = "[S]earch current [W]ord" })
-
-		vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics Document" })
-		vim.keymap.set("n", "<leader>sD", fzf.diagnostics_workspace, { desc = "[S]earch [D]iagnostics Workspace" })
-
-		vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
-
-		vim.keymap.set("n", "<C-p>", fzf.git_files, { desc = "[ ] Git files" })
+		-- vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
+		-- vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
+		-- vim.keymap.set("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect Telescope" })
+		-- vim.keymap.set("n", "<leader>sw", fzf.grep, { desc = "[S]earch current [W]ord" })
+		-- vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
+		--
+		-- vim.keymap.set("n", "<leader>sW", fzf.grep_cWORD, { desc = "[S]earch current [W]ord" })
+		--
+		-- vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
+		-- vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics Document" })
+		-- vim.keymap.set("n", "<leader>sD", fzf.diagnostics_workspace, { desc = "[S]earch [D]iagnostics Workspace" })
+		--
+		-- vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+		-- vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		-- vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
+		--
+		-- vim.keymap.set("n", "<C-p>", fzf.git_files, { desc = "[ ] Git files" })
 	end,
 }
