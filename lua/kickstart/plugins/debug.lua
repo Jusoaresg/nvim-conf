9ccea08da7f6c1505691649478c2575f3aa674bd
jusoaresg <julianosgreg@gmail.com>
2026-02-22 02:47:44 -0300

    feat(plugins): add dap-view and remove dap-ui

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,11 +1,3 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
 	-- NOTE: Yes, you can install new plugins here!
 	"mfussenegger/nvim-dap",
@@ -13,24 +5,18 @@ return {
 
 	-- NOTE: And you can specify dependencies as well
 	dependencies = {
-		-- Creates a beautiful debugger UI
-		"rcarriga/nvim-dap-ui",
+		"igorlfs/nvim-dap-view",
 
-		-- Required dependency for nvim-dap-ui
-		"nvim-neotest/nvim-nio",
-
-		-- Installs the debug adapters for you
 		"williamboman/mason.nvim",
 		"jay-babu/mason-nvim-dap.nvim",
 
-		-- Add your own debuggers here
 		"leoluz/nvim-dap-go",
-
 		"mfussenegger/nvim-dap-python",
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
+
+		local dapview = require("dap-view")
 		require("dap-python").setup("python")
 
 		require("mason-nvim-dap").setup({
@@ -45,8 +31,7 @@ return {
 			-- You'll need to check that you have the required things installed
 			-- online, please don't ask me how to install them :)
 			ensure_installed = {
-				-- Update this to ensure that you have the debuggers for the langs you want
-				"delve",
+				"netcoredbg",
 			},
 		})
 
@@ -60,36 +45,28 @@ return {
 			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 		end, { desc = "Debug: Set Breakpoint" })
 
-		-- Dap UI setup
-		-- For more information, see |:help nvim-dap-ui|
-		dapui.setup({
-			-- Set icons to characters that are more likely to work in every terminal.
-			--    Feel free to remove or use ones that you like more! :)
-			--    Don't feel like these are good choices.
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
+		dapview.setup({
+			winbar = {
+				controls = {
+					enabled = true,
+					position = "right",
+					buttons = {
+						"play",
+						"step_into",
+						"step_over",
+						"step_out",
+						"step_back",
+						"run_last",
+						"terminate",
+					},
 				},
 			},
 		})
 
-		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+		dap.listeners.after.event_initialized["dapui_config"] = dapview.open
+		dap.listeners.before.event_terminated["dapui_config"] = dapview.close
+		dap.listeners.before.event_exited["dapui_config"] = dapview.close
 
-		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		dap.listeners.before.event_exited["dapui_config"] = dapui.close
-
-		-- Install golang specific config
 		require("dap-go").setup()
 	end,
 }
