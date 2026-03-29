7e854b637b6044894062b9e2d525f1d6d18eda8a
jusoaresg <julianosgreg@gmail.com>
2026-03-29 17:40:39 -0300

    feat(debug): better debug keymaps

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -36,14 +36,11 @@ return {
 		})
 
 		-- Basic debugging keymaps, feel free to change to your liking!
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-		vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-		vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-		vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<leader>Bs", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<leader>Bi", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<leader>Bn", dap.step_over, { desc = "Debug: Step Over [Next]" })
+		vim.keymap.set("n", "<leader>Bo", dap.step_out, { desc = "Debug: Step Out" })
 		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		vim.keymap.set("n", "<leader>B", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end, { desc = "Debug: Set Breakpoint" })
 
 		dapview.setup({
 			winbar = {
