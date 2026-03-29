a6ebb628f9e29b912ec16d57b33b75888975b966
jusoaresg <julianosgreg@gmail.com>
2026-03-29 17:40:57 -0300

    feat(makeprg): using cargo build when editing .rs file

--- lua/custom/comments.lua
+++ lua/custom/comments.lua
@@ -14,4 +14,12 @@ return {
 			vim.bo.commentstring = "<!-- %s -->"
 		end,
 	}),
+
+	-- Make commands
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "rust",
+		callback = function()
+			vim.opt_local.makeprg = "cargo build"
+		end,
+	}),
 }
