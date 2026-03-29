191a54e007aaabcf3115399a7e961f08353ed2ad
jusoaresg <julianosgreg@gmail.com>
2026-03-29 17:39:06 -0300

    refactor(mini): refactor splitjoin config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,14 +1,16 @@
 return {
-	"nvim-mini/mini.splitjoin",
-	version = "*",
-	config = function()
-		local splitjoin = require("mini.splitjoin")
-		splitjoin.setup({
-			mappings = {
-				toggle = "m",
-				split = "",
-				join = "",
-			},
-		})
-	end,
+	{
+		"nvim-mini/mini.splitjoin",
+		version = "*",
+		config = function()
+			local splitjoin = require("mini.splitjoin")
+			splitjoin.setup({
+				mappings = {
+					toggle = "m",
+					split = "",
+					join = "",
+				},
+			})
+		end,
+	},
 }
